0a8405d3168ce9f1b5a9e5dac43d2cbb1403d6d3
Cobra-de1 <chuong11112001@gmail.com>
2025-04-21 17:23:24 +0700

    Upload plugin

---
+++ server.py
@@ -0,0 +1,204 @@
+import os
+import sys
+import ast
+import json
+import shutil
+import argparse
+import http.client
+
+from mcp.server.fastmcp import FastMCP
+
+# The log_level is necessary for Cline to work: https://github.com/jlowin/fastmcp/issues/81
+mcp = FastMCP("ida_pro_mcp", log_level="ERROR")
+
+jsonrpc_request_id = 1
+ida_host = "localhost"
+ida_port = 13337
+
+def make_jsonrpc_request(method: str, *params):
+    """Make a JSON-RPC request to the IDA plugin"""
+    global jsonrpc_request_id
+    conn = http.client.HTTPConnection(ida_host, ida_port)
+    request = {
+        "jsonrpc": "2.0",
+        "method": method,
+        "params": list(params),
+        "id": jsonrpc_request_id,
+    }
+    jsonrpc_request_id += 1
+
+    try:
+        conn.request("POST", "/mcp", json.dumps(request), {
+            "Content-Type": "application/json"
+        })
+        response = conn.getresponse()
+        data = json.loads(response.read().decode())
+
+        if "error" in data:
+            error = data["error"]
+            code = error["code"]
+            message = error["message"]
+            pretty = f"JSON-RPC error {code}: {message}"
+            if "data" in error:
+                pretty += "\n" + error["data"]
+            raise Exception(pretty)
+
+        result = data["result"]
+        # NOTE: LLMs do not respond well to empty responses
+        if result is None:
+            result = "success"
+        return result
+    except Exception:
+        raise
+    finally:
+        conn.close()
+
+@mcp.tool()
+def check_connection() -> str:
+    """Check if the IDA plugin is running"""
+    try:
+        metadata = make_jsonrpc_request("get_metadata")
+        return f"Successfully connected to IDA Pro (open file: {metadata['module']})"
+    except Exception as e:
+        if sys.platform == "darwin":
+            shortcut = "Ctrl+Option+M"
+        else:
+            shortcut = "Ctrl+Alt+M"
+        return f"Failed to connect to IDA Pro! Did you run Edit -> Plugins -> MCP ({shortcut}) to start the server?"
+
+# Code taken from https://github.com/mrexodia/ida-pro-mcp (MIT License)
+class MCPVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.types: dict[str, ast.ClassDef] = {}
+        self.functions: dict[str, ast.FunctionDef] = {}
+        self.descriptions: dict[str, str] = {}
+
+    def visit_FunctionDef(self, node):
+        for decorator in node.decorator_list:
+            if isinstance(decorator, ast.Name):
+                if decorator.id == "jsonrpc":
+                    for i, arg in enumerate(node.args.args):
+                        arg_name = arg.arg
+                        arg_type = arg.annotation
+                        if arg_type is None:
+                            raise Exception(f"Missing argument type for {node.name}.{arg_name}")
+                        if isinstance(arg_type, ast.Subscript):
+                            assert isinstance(arg_type.value, ast.Name)
+                            assert arg_type.value.id == "Annotated"
+                            assert isinstance(arg_type.slice, ast.Tuple)
+                            assert len(arg_type.slice.elts) == 2
+                            annot_type = arg_type.slice.elts[0]
+                            annot_description = arg_type.slice.elts[1]
+                            assert isinstance(annot_description, ast.Constant)
+                            node.args.args[i].annotation = ast.Subscript(
+                                value=ast.Name(id="Annotated", ctx=ast.Load()),
+                                slice=ast.Tuple(
+                                    elts=[
+                                    annot_type,
+                                    ast.Call(
+                                        func=ast.Name(id="Field", ctx=ast.Load()),
+                                        args=[],
+                                        keywords=[
+                                        ast.keyword(
+                                            arg="description",
+                                            value=annot_description)])],
+                                    ctx=ast.Load()),
+                                ctx=ast.Load())
+                        elif isinstance(arg_type, ast.Name):
+                            pass
+                        else:
+                            raise Exception(f"Unexpected type annotation for {node.name}.{arg_name} -> {type(arg_type)}")
+
+                    body_comment = node.body[0]
+                    if isinstance(body_comment, ast.Expr) and isinstance(body_comment.value, ast.Constant):
+                        new_body = [body_comment]
+                        self.descriptions[node.name] = body_comment.value.value
+                    else:
+                        new_body = []
+
+                    call_args = [ast.Constant(value=node.name)]
+                    for arg in node.args.args:
+                        call_args.append(ast.Name(id=arg.arg, ctx=ast.Load()))
+                    new_body.append(ast.Return(
+                        value=ast.Call(
+                            func=ast.Name(id="make_jsonrpc_request", ctx=ast.Load()),
+                            args=call_args,
+                            keywords=[])))
+                    decorator_list = [
+                        ast.Call(
+                            func=ast.Attribute(
+                                value=ast.Name(id="mcp", ctx=ast.Load()),
+                                attr="tool",
+                                ctx=ast.Load()),
+                            args=[],
+                            keywords=[]
+                        )
+                    ]
+                    node_nobody = ast.FunctionDef(node.name, node.args, new_body, decorator_list, node.returns, node.type_comment, lineno=node.lineno, col_offset=node.col_offset)
+                    self.functions[node.name] = node_nobody
+
+    def visit_ClassDef(self, node):
+        for base in node.bases:
+            if isinstance(base, ast.Name):
+                if base.id == "TypedDict":
+                    self.types[node.name] = node
+
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+IDA_PLUGIN_PY = os.path.join(SCRIPT_DIR, "mcp-plugin.py")
+GENERATED_PY = os.path.join(SCRIPT_DIR, "server_generated.py")
+
+# NOTE: This is in the global scope on purpose
+if not os.path.exists(IDA_PLUGIN_PY):
+    raise RuntimeError(f"IDA plugin not found at {IDA_PLUGIN_PY} (did you move it?)")
+with open(IDA_PLUGIN_PY, "r") as f:
+    code = f.read()
+module = ast.parse(code, IDA_PLUGIN_PY)
+visitor = MCPVisitor()
+visitor.visit(module)
+code = """# NOTE: This file has been automatically generated, do not modify!
+# Architecture based on https://github.com/mrexodia/ida-pro-mcp (MIT License)
+from typing import Annotated, Optional, TypedDict, Generic, TypeVar
+from pydantic import Field
+
+T = TypeVar("T")
+
+"""
+for type in visitor.types.values():
+    code += ast.unparse(type)
+    code += "\n\n"
+for function in visitor.functions.values():
+    code += ast.unparse(function)
+    code += "\n\n"
+with open(GENERATED_PY, "w") as f:
+    f.write(code)
+exec(compile(code, GENERATED_PY, "exec"))
+
+MCP_FUNCTIONS = ["check_connection"] + list(visitor.functions.keys())
+
+def main():
+    parser = argparse.ArgumentParser(description="IDA Pro MCP Server")
+    parser.add_argument("--host", default='0.0.0.0', help='Host to bind to')
+    parser.add_argument("--port", type=int, default=8080, help='Port to listen on')
+    parser.add_argument("--transport", default="stdio", choices=["stdio", "sse"], help="Transport mechanism (default: sse)")
+    parser.add_argument("--ida-host", default='localhost', help='Host of IDA Pro Plugin')
+    parser.add_argument("--ida-port", type=int, default=13337, help='Port of IDA Pro Plugin')
+    args = parser.parse_args()
+
+    if args.ida_host:
+        global ida_host
+        ida_host = args.ida_host
+    if args.ida_port:
+        global ida_port
+        ida_port = args.ida_port
+
+    if args.transport == "sse":
+        mcp.settings.host = args.host
+        mcp.settings.port = args.port
+        mcp.settings.sse_path = '/sse'
+        mcp.settings.message_path = '/messages/'
+    
+    mcp.run(transport=args.transport)
+
+if __name__ == "__main__":
+    main()
